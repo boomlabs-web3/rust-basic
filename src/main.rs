da0c7769be3a5b4dc9c52c87d1b82ce622058200
c0wjay <poppinjaewoo@gmail.com>
2022-09-18 22:18:30 +0900

    Implement MiniVec

--- src/main.rs
+++ src/main.rs
@@ -7,4 +7,26 @@ use ided::use_ided;
 mod student;
 use student::Student;
 
-fn main() {}
+#[derive(Debug)]
+struct MiniVec {
+    contents: Vec<u64>,
+}
+
+impl MiniVec {
+    fn new() -> Self {
+        Self {
+            contents: Vec::new(),
+        }
+    }
+
+    fn push(&mut self, item: u64) {
+        self.contents.push(item);
+    }
+}
+
+fn main() {
+    let mut v = MiniVec::new();
+    v.push(10);
+
+    println!("v = {:?}", v);
+}
