ad61248d6e31680bbf5b6af49f8cb2a43b899f32
c0wjay <poppinjaewoo@gmail.com>
2022-09-18 22:38:45 +0900

    Implement Generic structs

--- src/main.rs
+++ src/main.rs
@@ -8,18 +8,18 @@ mod student;
 use student::Student;
 
 #[derive(Debug)]
-struct MiniVec {
-    contents: Vec<u64>,
+struct MiniVec<T> {
+    contents: Vec<T>,
 }
 
-impl MiniVec {
+impl<T> MiniVec<T> {
     fn new() -> Self {
         Self {
             contents: Vec::new(),
         }
     }
 
-    fn push(&mut self, item: u64) {
+    fn push(&mut self, item: T) {
         self.contents.push(item);
     }
 }
@@ -28,5 +28,16 @@ fn main() {
     let mut v = MiniVec::new();
     v.push(10);
 
-    println!("v = {:?}", v);
+    let mut v2 = MiniVec::new();
+    v2.push('c');
+
+    let mut v3: MiniVec<char> = MiniVec::new();
+
+    let mut v4 = MiniVec::<Employee>::new();
+    v4.push(Employee::new("John".to_string(), 1));
+
+    println!(
+        "v = {:?}\n v2 = {:?}\n v3 = {:?}\n v4 = {:?}",
+        v, v2, v3, v4
+    );
 }
