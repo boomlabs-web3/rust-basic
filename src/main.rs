3fb09f7239684069fb6d14d4c77c515abb3b0351
c0wjay <poppinjaewoo@gmail.com>
2022-09-26 22:09:50 +0900

    Convert &str to String

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
-fn greet(x: &str) {
-    println!("Hello to {}", x);
+fn greet(x: &str) -> String {
+    format!("{}", x)
 }
 
 fn main() {
     let my_greeting = "Hello, world!";
-    greet(my_greeting);
+    let result = greet(my_greeting);
+    println!("{}", result);
 }
