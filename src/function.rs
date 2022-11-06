1b122d45d12883de524ec6baabaa854de0a7519b
c0wjay <poppinjaewoo@gmail.com>
2022-11-06 14:13:45 +0900

    Using Closure - Function #2

--- src/function.rs
+++ src/function.rs
@@ -9,22 +9,15 @@ fn simulated_expensive_calculation(intensity: u32) -> u32 {
 
 pub fn generate_workout(intensity: u32, random_number: u32) {
     if intensity < 10 {
-        println!(
-            "Today, do {} pushups!",
-            simulated_expensive_calculation(intensity)
-        );
-        println!(
-            "Next, do {} situps!",
-            simulated_expensive_calculation(intensity)
-        );
+        let expensive_result = simulated_expensive_calculation(intensity);
+        println!("Today, do {} pushups!", expensive_result);
+        println!("Next, do {} situps!", expensive_result);
     } else {
         if random_number == 0 {
             println!("Take a break today! Remember to stay hydrated!");
         } else {
-            println!(
-                "Today, run for {} minutes!",
-                simulated_expensive_calculation(intensity)
-            );
+            let expensive_result = simulated_expensive_calculation(intensity);
+            println!("Today, run for {} minutes!", expensive_result);
         }
     }
 }
