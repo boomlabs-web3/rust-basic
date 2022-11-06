1d851387149125bafbddf4cfa7f7bba366c54304
c0wjay <poppinjaewoo@gmail.com>
2022-11-06 14:12:51 +0900

    Using Closure - Function #1

---
+++ src/function.rs
@@ -0,0 +1,30 @@
+use std::thread;
+use std::time::Duration;
+
+fn simulated_expensive_calculation(intensity: u32) -> u32 {
+    println!("calculating slowly...");
+    thread::sleep(Duration::from_secs(2));
+    intensity
+}
+
+pub fn generate_workout(intensity: u32, random_number: u32) {
+    if intensity < 10 {
+        println!(
+            "Today, do {} pushups!",
+            simulated_expensive_calculation(intensity)
+        );
+        println!(
+            "Next, do {} situps!",
+            simulated_expensive_calculation(intensity)
+        );
+    } else {
+        if random_number == 0 {
+            println!("Take a break today! Remember to stay hydrated!");
+        } else {
+            println!(
+                "Today, run for {} minutes!",
+                simulated_expensive_calculation(intensity)
+            );
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,9 @@
-mod employee;
-use employee::{Employee, EmployeeRecords};
-
-mod printinfo;
+mod function;
+use function::generate_workout;
 
 fn main() {
-    let john = Employee::new(String::from("John"), 100);
-    let jane = Employee::new(String::from("Jane"), 101);
-    let tom = Employee::new(String::from("Tom"), 102);
+    let intensity = 5;
+    let random_number = 3;
 
-    let mut employee_records = EmployeeRecords::new();
-    employee_records.push(john);
-    EmployeeRecords::push(&mut employee_records, jane);
-    employee_records.push(tom);
-
-    println!("{:#?}", employee_records);
-    println!("Get 101: {:?}", employee_records.get(101));
-    println!("Get 100: {:?}", employee_records.get(100));
-    println!("---------------------------------------------\n");
-
-    println!("Length: {}", employee_records.clone().count());
-    println!("Last: {:?}", employee_records.last().unwrap());
-
-    // println!("{:#?}", employee_records);
-    // println!("Next: {:?}", employee_records.next());
-    // println!("---------------------------------------------\n");
-
-    // println!("{:#?}", employee_records);
-    // println!("Get 100: {:?}", employee_records.get(100));
-    // println!("---------------------------------------------\n");
-
-    // for employee in &mut employee_records {
-    //     println!("For {:#?}", employee);
-    // }
-    // println!("{:#?}", employee_records);
+    generate_workout(intensity, random_number);
 }
