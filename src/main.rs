2b536de77290cc521b6925cc0c2663423efc7db8
c0wjay <poppinjaewoo@gmail.com>
2022-11-13 13:37:36 +0900

    Source code of ptr::write

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
-fn main() {
-    use std::mem;
-
-    let mut v = vec![65, 122];
-    // `v`의 내용으로 Sting `s`를 생성
-    let s = unsafe { String::from_raw_parts(v.as_mut_ptr(), v.len(), v.capacity()) };
-    println!("{:?}", v);
-    // `v`의 메모리는 `s`에 의해 관리되기 때문에 leak.
-    mem::forget(v); // 이 부분을 주석처리하면 double-free error가 생김.
-    assert_eq!(s, "Az");
-    mem::drop(s); // `s` 는 drop되고, 메모리는 dealloc됨.
+#[inline]
+#[stable(feature = "rust1", since = "1.0.0")]
+#[rustc_const_unstable(feature = "const_ptr_write", issue = "86302")]
+#[cfg_attr(miri, track_caller)]
+pub const unsafe fn write<T>(dst: *mut T, src: T) {
+    extern "rust-intrinsic" {
+        #[rustc_const_stable(feature = "const_intrinsic_copy", since = "1.63.0")]
+        fn copy_nonoverlapping<T>(src: *const T, dst: *mut T, count: usize);
+    }
+    unsafe {
+        copy_nonoverlapping(&src as *const T, dst, 1); // src에 대한 새로운 pointer 생성
+        intrinsics::forget(src); // 여기서 forget하지 않으면 double-free
+    }
 }
